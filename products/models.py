3a1cea783c9ceffb9e68de9284c44b86b94aa161
Peniel Negasa <penaye779@gmail.com>
2026-02-24 12:14:38 +0300

    Initial commit

---
+++ products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from categories.models import Category
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
