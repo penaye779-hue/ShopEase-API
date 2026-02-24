ff2ff00fb6abcbd57f47980695d3d634677a95bf
Peniel Negasa <penaye779@gmail.com>
2026-02-24 13:04:14 +0300

    Initial Django project setup with apps and migrations

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserRegisterSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserRegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
