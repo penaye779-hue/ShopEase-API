d8192d0462fa863c49af5cc3ca6fa2a2bffc24b9
Peniel Negasa <penaye779@gmail.com>
2026-02-25 12:48:40 +0300

    ShopEase backend: authentication, product and category CRUD completed

--- products/views.py
+++ products/views.py
@@ -2,7 +2,9 @@
 from rest_framework import generics, permissions
 from .models import Product
 from .serializers import ProductSerializer
-
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -12,3 +14,12 @@ class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['category']
+    search_fields = ['name']    
--- shopease/settings.py
+++ shopease/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'users',
     'products',
     'categories',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -123,5 +124,11 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
     ),
 }
