2a885133e80c7319a71a3dd754804ebeed30793d
Peniel Negasa <penaye779@gmail.com>
2026-02-26 20:08:23 +0300

    Added product ownership system with custom permissions (IsOwnerOrReadOnly)

--- products/models.py
+++ products/models.py
@@ -1,15 +1,16 @@
 # products/models.py
 from django.db import models
 from categories.models import Category
+from django.contrib.auth.models import User
 
 class Product(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    image_url = models.URLField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Pagination class
 class ProductPagination(PageNumberPagination):
@@ -26,3 +27,12 @@ class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Anyone can view, only auth users can update/delete
+from .permissions import IsOwnerOrReadOnly
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
